40e9a1dc7ec21bb1bd5194a96722cfd703cf217b
Mohammadmohebi33 <mohammad.rasoul.mohebi@gmail.com>
2025-12-27 15:41:02 +0330

    first commit

---
+++ go.mod
@@ -0,0 +1,10 @@
+module flexy
+
+go 1.24.4
+
+require (
+	github.com/golang-jwt/jwt/v5 v5.3.0 // indirect
+	github.com/joho/godotenv v1.5.1 // indirect
+	github.com/mattn/go-sqlite3 v1.14.32 // indirect
+	golang.org/x/crypto v0.46.0 // indirect
+)
---
+++ models/user.go
@@ -0,0 +1,23 @@
+package models
+
+import (
+	"time"
+)
+
+type User struct {
+	ID              int        `json:"id"`
+	Name            string     `json:"name"`
+	Email           *string    `json:"email"`
+	EmailVerifiedAt *time.Time `json:"email_verified_at"`
+	Password        string     `json:"-"`
+	ScoreWeek       int        `json:"score_week"`
+	ScoreMonth      int        `json:"score_month"`
+	Score           int        `json:"score"`
+	AvatarURL       *string    `json:"avatar_url"`
+	Status          string     `json:"status"`
+	ActiveDays      int        `json:"active_days"`
+	LastActiveAt    *time.Time `json:"last_active_at"`
+	RememberToken   *string    `json:"remember_token"`
+	CreatedAt       time.Time  `json:"created_at"`
+	UpdatedAt       time.Time  `json:"updated_at"`
+}
