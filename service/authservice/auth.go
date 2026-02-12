881883f805298b8ae42505ce4415c3c1f2c635cf
Mohammadmohebi33 <mohammad.rasoul.mohebi@gmail.com>
2026-02-12 23:21:33 +0330

    save otp code in redis

--- service/authservice/auth.go
+++ service/authservice/auth.go
@@ -3,7 +3,11 @@ package authservice
 import (
 	"crypto/md5"
 	"encoding/hex"
+	"flexy/adapter/redis"
 	"flexy/entity"
+	"fmt"
+	"math/rand"
+	"time"
 )
 
 type Repository interface {
@@ -11,14 +15,29 @@ type Repository interface {
 }
 
 type Service struct {
-	repo Repository
+	repo         Repository
+	redisAdapter redis.Adapter
 }
 
 func New(repo Repository) Service {
-	return Service{repo: repo}
+	return Service{
+		repo: repo,
+		redisAdapter: redis.New(
+			redis.Config{
+				Host:     "127.0.0.1",
+				Port:     6379,
+				Password: "",
+				DB:       0,
+			}),
+	}
 }
 
 func getMD5Hash(text string) string {
 	hash := md5.Sum([]byte(text))
 	return hex.EncodeToString(hash[:])
+}
+
+func generateOTP() string {
+	rand.Seed(time.Now().UnixNano())
+	return fmt.Sprintf("%06d", rand.Intn(1000000))
 }
--- service/authservice/register.go
+++ service/authservice/register.go
@@ -1,9 +1,11 @@
 package authservice
 
 import (
+	"context"
 	"flexy/dto"
 	"flexy/entity"
 	"fmt"
+	"time"
 )
 
 func (s Service) Register(req dto.RegisterRequest) (dto.RegisterResponse, error) {
@@ -18,6 +20,17 @@ func (s Service) Register(req dto.RegisterRequest) (dto.RegisterResponse, error)
 	createdUser, err := s.repo.RegisterUser(user)
 	if err != nil {
 		return dto.RegisterResponse{}, fmt.Errorf("unexpected error: %w", err)
+	}
+
+	otp := generateOTP()
+
+	key := fmt.Sprintf("verify:%d", createdUser.ID)
+
+	ctx := context.Background()
+
+	err = s.redisAdapter.Client().Set(ctx, key, otp, 3*time.Minute).Err()
+	if err != nil {
+		return dto.RegisterResponse{}, fmt.Errorf("failed to store otp: %w", err)
 	}
 
 	return dto.RegisterResponse{
